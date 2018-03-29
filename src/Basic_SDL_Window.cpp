4a50da979e933040de3a39d2ef65bce36063f7f2
jpetterson <jpetterson@DS-17.decker.local>
2018-03-29 09:36:07 -0500

    3/29/18

--- src/Basic_SDL_Window.cpp
+++ src/Basic_SDL_Window.cpp
@@ -15,26 +15,28 @@ int main(int argc, char *argv[])
 
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 600;
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 	cout << "SDL init failed." << endl;
 	return 1;
 	}
-
+    //SDL object
     SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
 	    SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-    if (window == NULL)
+    if (window == NULL) //if window doesn't load, run the quit function
 	{
 	SDL_Quit();
-	return 2;
+	return 2; // return a number so you can look at where things went wrong in error codes/0 if runs good
 	}
+
     bool quit = false;
 
     SDL_Event event;
 
-    while (!quit)
+    while (!quit)	//game loop
 	{
 	//update particles
 	//draw particles
@@ -49,8 +51,9 @@ int main(int argc, char *argv[])
 	    }
 	}
 
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(window); //kills the anything to do with the window
 
-    SDL_Quit();
-    return 0;
+    SDL_Quit(); //quit SDL stuff
+
+    return 0; //return 0 b/c all ran well
     }
